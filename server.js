380ac05414f60b998b9ecd5423f6b59b96072837
mariop <partidam578@gmail.com>
2024-02-17 18:00:43 -0700

    Combine

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ const path = require("path");
 dotenv.config();
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "HTMLANDCSS")));
 
 app.get("/", (req, res) => {
-  res.send("HELLO WORLDDD");
+  res.render("index");
 });
 
 app.listen(PORT, () => {
